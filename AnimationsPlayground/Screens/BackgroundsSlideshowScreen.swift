1f4dfc0c31ca4e5ed9205c5ae26c277f87338f2b
Fernando Draghi <ferna182@gmail.com>
2024-11-26 12:49:55 -0300

    Apparently TabView enters an infinite loop when loading during a transition and setting tabViewStyle. So... A hack it is.

--- AnimationsPlayground/Screens/BackgroundsSlideshowScreen.swift
+++ AnimationsPlayground/Screens/BackgroundsSlideshowScreen.swift
@@ -8,38 +8,46 @@
 import SwiftUI
 
 struct BackgroundsSlideshowScreen: View {
-    let imageNames = Array(1...13).map{"img\($0)"}
-    
     @State private var currentBkg = 0
+    @State private var loadTabView = false
     @State private var hueAnimation = false
+    private let imageNames = Array(1...13).map{"img\($0)"}
+
     private let timer = Timer.publish(every: 5, on: .main, in: .default).autoconnect()
     
     var body: some View {
         GeometryReader { g in
             VStack {
-                ZStack {
+                if (loadTabView) {
                     TabView(selection: $currentBkg) {
                         ForEach(imageNames.indices, id:\.self) { index in
                             Image(imageNames[index])
                                 .resizable()
                                 .scaledToFill()
-                                .frame(width: g.size.width, height: g.size.height)
                                 .tag(index)
                         }
                     }
+                    .tabViewStyle(.page)
                     .hueRotation(.degrees(hueAnimation ? 720 : 0))
                     .padding(.bottom, 0)
-                    .tabViewStyle(.page(indexDisplayMode: .always))
                     .animation(.easeInOut(duration:1), value: currentBkg)
                     .animation(.linear(duration: 5).repeatForever(), value: hueAnimation)
                 }
             }
+            .padding(.top, 20)
         }
         .onReceive(timer) { _ in
             currentBkg = (currentBkg + 1) % max(imageNames.count, 1)
         }
         .onAppear() {
-            hueAnimation.toggle()
+            //So... Apparently TabView is broken when using tabViewStyle and loading during transitions?
+            //So I have to do this crap?
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
+                loadTabView = true
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
+                    hueAnimation.toggle()
+                }
+            }
         }
         .navigationTitle("Background Slideshow")
     }

14a76ea7df2cb6479a152c181c3d6b9f78fec7ca
Fernando Draghi <ferna182@gmail.com>
2024-11-26 01:28:05 -0300

    Timer is the way to go. Record Player screen is functional now.

--- AnimationsPlayground/Screens/RecordPlayerScreen.swift
+++ AnimationsPlayground/Screens/RecordPlayerScreen.swift
@@ -17,54 +17,24 @@ enum RecordPlayerState {
 }
 
 struct RecordPlayerScreen: View {
-    @State private var state: RecordPlayerState = .stopped
-    @State private var audioPlayer: AVAudioPlayer?
     @State private var play: Bool = false
-    private var songDuration: Double {
-        audioPlayer?.duration ?? 0
-    }
-    
-    private var currentTime: Double {
-        audioPlayer?.currentTime ?? 0
-    }
-    
-    private var isPlaying: Bool {
-        state == .starting || state == .playing
-    }
     
     var body: some View {
         VStack {
             Text("This screen is broken as of now.")
                 .font(.title)
                 .multilineTextAlignment(.leading)
-            RecordPlayer(state: $state, player: $audioPlayer, play: $play)
+            RecordPlayer(play: $play)
                 .padding(.top, 40)
             Button {
                 play.toggle()
-                if state == .stopped ||
-                    state == .stopping {
-                    state = .starting
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-                        state = .playing
-                        audioPlayer?.play()
-                    }
-                } else {
-                    state = .stopping
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                        audioPlayer?.stop()
-                        audioPlayer?.currentTime = 0
-                    }
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-                        state = .stopped
-                    }
-                }
             } label: {
                 HStack(spacing: 8) {
-                    Text(isPlaying ? "Stop " : "Play ")
+                    Text(play ? "Stop " : "Play ")
                         .bold()
                         .font(.title2)
                         .foregroundStyle(.white)
-                    Image(systemName: isPlaying ? "stop.fill" : "play.circle.fill")
+                    Image(systemName: play ? "stop.fill" : "play.circle.fill")
                         .foregroundStyle(.white)
                         .imageScale(.large)
                 }
@@ -73,30 +43,10 @@ struct RecordPlayerScreen: View {
                 .background(Capsule().strokeBorder(.white, lineWidth: 2))
                 
             }
-            .disabled(state == .starting || state == .stopping)
-            .opacity((state == .starting || state == .stopping) ? 0.3 : 1)
             .padding(.top, 80)
             Spacer()
         }
-        .onAppear() {
-            loadMusic()
-        }
         .navigationTitle("Record Player")
-    }
-    
-    private func loadMusic() {
-        guard let fileUrl = Bundle.main.path(forResource: "music", ofType: "m4a") else {
-            print("Audio file not found!")
-            return
-        }
-        do {
-            audioPlayer = try AVAudioPlayer(contentsOf: URL(filePath: fileUrl))
-
-            audioPlayer?.numberOfLoops = 1
-            //audioPlayer?.volume = 0.1
-        } catch {
-            print("Audioplayer failed: \(error)")
-        }
     }
 }
 
@@ -110,47 +60,36 @@ struct PlayerArmAnimationValues {
 }
 
 struct RecordPlayer: View {
-    @Binding var state: RecordPlayerState
-    @Binding var player: AVAudioPlayer?
     @Binding var play: Bool
+    @State private var player: AVAudioPlayer? = nil
+    @State private var state: RecordPlayerState = .stopped
     
     @State private var timer: AnyCancellable?
+    @State private var stateChangeTimer: Timer?
+    
+    // Record Properties
     @State private var currentRotationDegrees: CGFloat = 0.0
     @State private var speed: CGFloat = 0.0
-
-    private var armPosition: Double {
-        switch state {
-        case .playing, .stopping:
-            return -27
-        default:
-            return -35
-        }
-    }
-
-    private var armTargetPosition: Double {
-        switch state {
-        case .starting, .playing:
-            return -27
-        default:
-            return -35
-        }
-    }
-
+    // Arm Properties
+    @State private var armScale = 1.0
+    @State private var armDegrees = -35.0
+    @State private var elapsedTime = 0.0
+    @State private var armRotationSpeed = 0.0
+    
+    private let armScaleStep = 0.05 / 60.0
+    private let armRotationStep = 23.0 / 120.0
+    private let armRotationSpeedAcceleration = (23.0 / 120.0) / 120.0
+    private let timeToStartStop = 5.0
+    @State private var armPlaybackSpeed = 0.0
+    @State private var songPlaybackCountdown = 0.0
+    // arm parking: -35, record start: -27, record end: -12
+    
     private var duration: Double {
         player?.duration ?? 0
     }
-
+    
     private var currentTime: Double {
         player?.currentTime ?? 0
-    }
-
-    private var armPlayingAnimation: Animation {
-        switch state {
-        case .playing:
-                .linear(duration: duration)
-        default:
-                .linear(duration: 0)
-        }
     }
     
     var body: some View {
@@ -171,24 +110,8 @@ struct RecordPlayer: View {
                 Image("playerArm")
                     .resizable()
                     .frame(width: 150, height: 150)
-                    .keyframeAnimator(initialValue: PlayerArmAnimationValues(rotation: Angle(degrees: armPosition)),
-                                      trigger: state,
-                                      content: { view, values in
-                        view
-                            .rotationEffect(values.rotation, anchor: .topTrailing)
-                            .scaleEffect(values.scale, anchor: .topTrailing)
-                    }, keyframes: { _ in
-                        KeyframeTrack(\.scale) {
-                            LinearKeyframe((state == .starting || state == .stopping) ?  1.05 : 1.0, duration: 1.0, timingCurve: .easeInOut)
-                            LinearKeyframe((state == .starting || state == .stopping) ?  1.05 : 1.0, duration: 3.0, timingCurve: .linear)
-                            LinearKeyframe(1.0, duration: 1.0, timingCurve: .easeIn)
-                        }
-
-                        KeyframeTrack(\.rotation) {
-                            LinearKeyframe(Angle(degrees: armPosition), duration: 1.0)
-                            LinearKeyframe(Angle(degrees: armTargetPosition), duration: 3.0, timingCurve: .easeInOut)
-                        }
-                    })
+                    .rotationEffect(.degrees(armDegrees), anchor: .topTrailing)
+                    .scaleEffect(armScale, anchor: .topTrailing)
                     .padding(.leading, 130)
                     .padding(.bottom, 90)
                     .shadow(color: .black, radius: 5, x: 2, y: 2)
@@ -197,14 +120,22 @@ struct RecordPlayer: View {
         }
         .onChange(of: play, { oldValue, newValue in
             if newValue {
-                startTimer()
+                startPlayback()
+            } else {
+                stopPlayback()
             }
         })
         .onChange(of: state) { oldValue, newValue in
+            if newValue == .starting {
+                startTimer()
+            }
             if newValue == .stopped {
                 timer?.cancel()
                 timer = nil
             }
+        }
+        .onAppear() {
+            loadMusic()
         }
     }
     
@@ -218,18 +149,97 @@ struct RecordPlayer: View {
             .autoconnect()
             .receive(on: DispatchQueue.main)
             .sink { _ in
-                if play {
+                switch state {
+                case .starting:
+                    elapsedTime += 1/60
+                    if elapsedTime < 1.0 {
+                        armScale = min(armScale + armScaleStep, 1.05)
+                    } else if elapsedTime < 4.0 {
+                        armRotationSpeed = min(armRotationSpeed + armRotationSpeedAcceleration, armRotationStep)
+                        armDegrees = min(armDegrees + armRotationSpeed, -27)
+                    } else {
+                        armScale = max(armScale - armScaleStep, 1.0)
+                    }
                     speed = min(speed + 0.05, 10.0)
-                } else {
+                case .playing:
+                    speed = min(speed + 0.05, 10.0)
+                    armDegrees = min(armDegrees + armPlaybackSpeed, -12)
+                    songPlaybackCountdown -= 1/60
+                    if songPlaybackCountdown <= 0 {
+                        stopPlayback()
+                    }
+                case .stopping:
+                    elapsedTime += 1/60
+                    if elapsedTime < 1.0 {
+                        armScale = min(armScale + armScaleStep, 1.05)
+                    } else if elapsedTime < 4.0 {
+                        armRotationSpeed = min(armRotationSpeed + armRotationSpeedAcceleration, armRotationStep)
+                        armDegrees = max(armDegrees - armRotationSpeed, -35)
+                    } else {
+                        armScale = max(armScale - armScaleStep, 1.0)
+                    }
                     speed = max(speed - 0.05, 0.0)
+                case .stopped:
+                    speed = 0
                 }
+                
+                // Modulus operation not available here... So...
                 if currentRotationDegrees > 360 {
                     currentRotationDegrees -= 360
                 }
+                
                 withAnimation {
                     currentRotationDegrees += speed
                 }
             }
+    }
+    
+    private func startPlayback() {
+        guard state == .stopping || state == .stopped else { return }
+        stateChangeTimer?.invalidate()
+        stateChangeTimer = nil
+        state = .starting
+        elapsedTime = 0
+        armRotationSpeed = 0
+        songPlaybackCountdown = player?.duration ?? 0.0
+        stateChangeTimer = Timer
+            .scheduledTimer(withTimeInterval: timeToStartStop, repeats: false, block: { _ in
+                state = .playing
+                player?.play()
+            })
+    }
+    
+    private func stopPlayback() {
+        guard state == .playing || state == .starting else { return }
+        stateChangeTimer?.invalidate()
+        stateChangeTimer = nil
+        state = .stopping
+        player?.stop()
+        player?.currentTime = 0
+        elapsedTime = 0
+        armRotationSpeed = 0
+        play = false
+        stateChangeTimer = Timer
+            .scheduledTimer(withTimeInterval: timeToStartStop, repeats: false, block: { _ in
+                state = .stopped
+            })
+    }
+    
+    private func loadMusic() {
+        guard let fileUrl = Bundle.main.path(forResource: "music", ofType: "m4a") else {
+            print("Audio file not found!")
+            return
+        }
+        do {
+            player = try AVAudioPlayer(contentsOf: URL(filePath: fileUrl))
+            
+            player?.numberOfLoops = 0
+            
+            let songDuration = player?.duration ?? 1.0
+            armPlaybackSpeed = (15.0 / songDuration) / 60.0
+        } catch {
+            print("Audioplayer failed: \(error)")
+        }
     }
 }
 
